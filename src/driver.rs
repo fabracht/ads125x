5b80e436a607cede33f9de8551f3e930aa59cf3a
Fabrício Bracht <fabracht@gmail.com>
2024-09-17 13:51:13 -0700

    decrease interval

--- src/driver.rs
+++ src/driver.rs
@@ -196,12 +196,12 @@ where
 
     /// Waits for DRDY pin to go low
     fn wait_for_drdy(&mut self) -> Result<(), Ads1256Error<SpiError, GpioError>> {
-        let timeout = 1000;
+        let timeout = 10000;
         for _ in 0..timeout {
             if self.drdy.is_low().map_err(Ads1256Error::Gpio)? {
                 return Ok(());
             }
-            self.delay.delay_us(250);
+            self.delay.delay_ms(1);
         }
         log::error!("DRDY pin did not go low");
         Err(Ads1256Error::Timeout)
@@ -210,9 +210,9 @@ where
     fn wait_for_drdy_high(&mut self) -> Result<(), Ads1256Error<SpiError, GpioError>> {
         let mut attempts = 0;
         while self.drdy.is_low().map_err(Ads1256Error::Gpio)? {
-            self.delay.delay_us(250);
+            self.delay.delay_us(1);
             attempts += 1;
-            if attempts > 1000 {
+            if attempts > 10000 {
                 log::error!("DRDY pin did not go high");
                 return Err(Ads1256Error::Timeout);
             }
