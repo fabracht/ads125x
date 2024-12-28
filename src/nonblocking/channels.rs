8096c99d613bd02e251498c01a98882cb2afd9a1
Fabrício Bracht <fabracht@gmail.com>
2024-12-28 19:42:44 +0000

    refactor: replace heapless Vec with fixed-size array for channel management

--- src/nonblocking/channels.rs
+++ src/nonblocking/channels.rs
@@ -8,7 +8,6 @@ use crate::{
 };
 use embedded_hal::digital::{InputPin, OutputPin};
 use embedded_hal_async::{delay::DelayNs, spi::SpiDevice};
-use heapless::Vec;
 
 const MAX_CHANNELS: usize = 8;
 
@@ -20,7 +19,7 @@ where
     PDWN: OutputPin<Error = GpioError>,
     DELAY: DelayNs,
 {
-    /// Change channel with non-blocking operations
+    /// Change channel operation
     pub async fn change_channel(
         &mut self,
         channel: u8,
@@ -49,7 +48,7 @@ where
         Ok(())
     }
 
-    /// Efficient channel cycling implementation
+    /// Efficient channel cycling implementation as per DataSheet
     pub async fn cycle_channel(
         &mut self,
         channel: u8,
@@ -86,22 +85,25 @@ where
         &mut self,
         channels: &[u8],
     ) -> ChannelSequencer<'_, SPI, CS, DRDY, PDWN, DELAY> {
-        let mut seq_channels: Vec<u8, MAX_CHANNELS> = Vec::new();
-        for &ch in channels.iter().take(MAX_CHANNELS) {
+        let mut seq_channels = [0u8; MAX_CHANNELS];
+        let mut length = 0;
+        for (index, &ch) in channels.iter().take(MAX_CHANNELS).enumerate() {
             if ch <= 7 {
                 // Ignore invalid channels
-                let _ = seq_channels.push(ch);
+                let _ = seq_channels[index] = ch;
+                length += 1;
             }
         }
-        ChannelSequencer::new(self, seq_channels)
+        ChannelSequencer::new(self, seq_channels, length)
     }
 }
 
 /// Channel sequencer for automated channel cycling
 pub struct ChannelSequencer<'a, SPI, CS, DRDY, PDWN, DELAY> {
     adc: &'a mut Ads1256NonBlocking<SPI, CS, DRDY, PDWN, DELAY>,
-    channels: Vec<u8, MAX_CHANNELS>,
+    channels: [u8; MAX_CHANNELS],
     current_index: usize,
+    length: usize,
 }
 
 impl<'a, SPI, CS, DRDY, PDWN, DELAY> ChannelSequencer<'a, SPI, CS, DRDY, PDWN, DELAY>
@@ -114,12 +116,14 @@ where
 {
     fn new(
         adc: &'a mut Ads1256NonBlocking<SPI, CS, DRDY, PDWN, DELAY>,
-        channels: Vec<u8, MAX_CHANNELS>,
+        channels: [u8; MAX_CHANNELS],
+        length: usize,
     ) -> Self {
         Self {
             adc,
             channels,
             current_index: 0,
+            length,
         }
     }
 
@@ -133,7 +137,7 @@ where
         DRDY: InputPin<Error = GpioError>,
         PDWN: OutputPin<Error = GpioError>,
     {
-        if self.current_index >= self.channels.len() {
+        if self.current_index >= self.length {
             self.current_index = 0;
             if self.channels.is_empty() {
                 return None;
