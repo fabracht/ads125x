5315adf4ac95f07748a3a28ea64be80f346450dd
Fabrício Bracht <fabracht@gmail.com>
2025-01-20 19:41:29 -0800

    feat: enhance async register printing with detailed bit values and names

--- src/nonblocking/calibration.rs
+++ src/nonblocking/calibration.rs
@@ -131,24 +131,119 @@ where
 
     /// Print all register values for debugging
     pub async fn print_registers(&mut self) -> Result<(), Ads1256Error<SpiError, GpioError>> {
+        // Read all registers first
         let registers = [
-            (REG_STATUS, "STATUS"),
-            (REG_MUX, "MUX"),
-            (REG_ADCON, "ADCON"),
-            (REG_DRATE, "DRATE"),
-            (REG_IO, "IO"),
-            (REG_OFC0, "OFC0"),
-            (REG_OFC1, "OFC1"),
-            (REG_OFC2, "OFC2"),
-            (REG_FSC0, "FSC0"),
-            (REG_FSC1, "FSC1"),
-            (REG_FSC2, "FSC2"),
+            (
+                REG_STATUS,
+                "STATUS",
+                ["ID3", "ID2", "ID1", "ID0", "ORDER", "ACAL", "BUFEN", "DRDY"],
+            ),
+            (
+                REG_MUX,
+                "MUX",
+                [
+                    "PSEL3", "PSEL2", "PSEL1", "PSEL0", "NSEL3", "NSEL2", "NSEL1", "NSEL0",
+                ],
+            ),
+            (
+                REG_ADCON,
+                "ADCON",
+                [
+                    "0", "CLK1", "CLK0", "SDCS1", "SDCS0", "PGA2", "PGA1", "PGA0",
+                ],
+            ),
+            (
+                REG_DRATE,
+                "DRATE",
+                ["DR7", "DR6", "DR5", "DR4", "DR3", "DR2", "DR1", "DR0"],
+            ),
+            (
+                REG_IO,
+                "IO",
+                [
+                    "DIR3", "DIR2", "DIR1", "DIR0", "DIO3", "DIO2", "DIO1", "DIO0",
+                ],
+            ),
+            (
+                REG_OFC0,
+                "OFC0",
+                [
+                    "OFC07", "OFC06", "OFC05", "OFC04", "OFC03", "OFC02", "OFC01", "OFC00",
+                ],
+            ),
+            (
+                REG_OFC1,
+                "OFC1",
+                [
+                    "OFC15", "OFC14", "OFC13", "OFC12", "OFC11", "OFC10", "OFC09", "OFC08",
+                ],
+            ),
+            (
+                REG_OFC2,
+                "OFC2",
+                [
+                    "OFC23", "OFC22", "OFC21", "OFC20", "OFC19", "OFC18", "OFC17", "OFC16",
+                ],
+            ),
+            (
+                REG_FSC0,
+                "FSC0",
+                [
+                    "FSC07", "FSC06", "FSC05", "FSC04", "FSC03", "FSC02", "FSC01", "FSC00",
+                ],
+            ),
+            (
+                REG_FSC1,
+                "FSC1",
+                [
+                    "FSC15", "FSC14", "FSC13", "FSC12", "FSC11", "FSC10", "FSC09", "FSC08",
+                ],
+            ),
+            (
+                REG_FSC2,
+                "FSC2",
+                [
+                    "FSC23", "FSC22", "FSC21", "FSC20", "FSC19", "FSC18", "FSC17", "FSC16",
+                ],
+            ),
         ];
 
-        for (reg, name) in registers.iter() {
+        // Print header
+        log::info!("Register Map:");
+        log::info!("ADDRESS REGISTER VALUE  BIT7  BIT6  BIT5  BIT4  BIT3  BIT2  BIT1  BIT0");
+        log::info!("------- -------- ------ ----- ----- ----- ----- ----- ----- ----- -----");
+
+        for (reg, name, bits) in registers.iter() {
             let mut buffer = [0u8; 1];
             self.read_register(*reg, &mut buffer).await?;
-            log::debug!("Register {}: 0x{:02X}", name, buffer[0]);
+            let value = buffer[0];
+
+            // Print register info with bit values
+            log::info!("{:02X}h     {:6}   0x{:02X}   {:4}   {:4}   {:4}   {:4}   {:4}   {:4}   {:4}   {:4}", 
+                reg, name, value,
+                if (value & 0x80) != 0 {"1"} else {"0"},
+                if (value & 0x40) != 0 {"1"} else {"0"},
+                if (value & 0x20) != 0 {"1"} else {"0"},
+                if (value & 0x10) != 0 {"1"} else {"0"},
+                if (value & 0x08) != 0 {"1"} else {"0"},
+                if (value & 0x04) != 0 {"1"} else {"0"},
+                if (value & 0x02) != 0 {"1"} else {"0"},
+                if (value & 0x01) != 0 {"1"} else {"0"}
+            );
+
+            // Print bit names
+            log::info!(
+                "                     {:4}   {:4}   {:4}   {:4}   {:4}   {:4}   {:4}   {:4}",
+                bits[0],
+                bits[1],
+                bits[2],
+                bits[3],
+                bits[4],
+                bits[5],
+                bits[6],
+                bits[7]
+            );
+            log::info!("");
         }
 
         Ok(())
