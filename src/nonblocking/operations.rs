84de15dc83cbff878dd196e2505768fc19576b28
Fabrício Bracht <fabracht@gmail.com>
2025-01-25 12:46:43 -0800

    minor spacing fix

--- src/nonblocking/operations.rs
+++ src/nonblocking/operations.rs
@@ -8,9 +8,9 @@ impl<SPI, CS, DRDY, PDWN, DELAY, SpiError, GpioError>
     crate::nonblocking::Ads1256NonBlocking<SPI, CS, DRDY, PDWN, DELAY>
 where
     SPI: SpiDevice<Error = SpiError>,
-    CS:OutputPin<Error = GpioError>,
+    CS: OutputPin<Error = GpioError>,
     DRDY: Wait + InputPin<Error = GpioError>,
-    PDWN:OutputPin<Error = GpioError>,
+    PDWN: OutputPin<Error = GpioError>,
     DELAY: DelayNs,
 {
     /// Sends a command to the ADS1256
